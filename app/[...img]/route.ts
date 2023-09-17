d08e051dd1b6e825128354d18a20510b0a44f69b
Eze Gatica <git@ezegatica.com>
2023-09-17 15:33:08 -0300

    Añadiendo proxy

--- app/[...img]/route.ts
+++ app/[...img]/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET(_request: Request, {params}: {params: {img: string}}) {
-    const img = await fetch('https://gatica.sirv.com/img/' + params.img);
+    const img = await fetch('https://gatica.sirv.com/img/' + params.img,{
+        cache: 'force-cache'
+    });
     const imgBuffer = await img.arrayBuffer();
     return new NextResponse(imgBuffer, {
         headers: {
---
+++ app/proxy/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, {params}: {params: {img: string}}) {
+    const { searchParams } = new URL(request.url)
+    const url = searchParams.get('url')
+
+    if (!url) {
+        return new NextResponse('url is required', {
+            status: 400
+        })
+    }
+    
+    const img = await fetch(url, {
+        cache: 'force-cache'
+    });
+
+    if (img.status === 404) {
+        return new NextResponse('Not found', {
+            status: 404
+        })
+    }
+
+    if (!img.headers.get('content-type')?.startsWith('image')) {
+        return new NextResponse('Not an image', {
+            status: 400
+        })
+    }
+
+    const imgBuffer = await img.arrayBuffer();
+
+    return new NextResponse(imgBuffer, {
+        headers: {
+            'Content-Type': 'image/png',
+            'Cache-Control': 'public, max-age=31536000, immutable'
+        }
+    });
+}
