99e8bd253d1619bdb8275caed15ee7906d944243
Eze Gatica <git@ezegatica.com>
2024-06-20 13:04:50 -0300

    Agregando resizing a las imagenes

--- app/cuadros/[...img]/route.ts
+++ app/cuadros/[...img]/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 
-export async function GET(_request: Request, {params}: {params: {img: string}}) {
-    const img = await fetch('https://gatica.sirv.com/public/cuadros/' + params.img,{
+export async function GET(request: Request, {params}: {params: {img: string}}) {
+    const queryParams = new URLSearchParams(request.url.split('?')[1]);
+    const url = 'https://gatica.sirv.com/public/cuadros/' + params.img + '?' + queryParams.toString();
+    const img = await fetch(url ,{
         cache: 'force-cache'
     });
     const imgBuffer = await img.arrayBuffer();
